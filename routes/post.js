69dcb539fc49cca5f6605db2e3a0fe33d6b3d681
arjun <arjun259194@gmail.com>
2022-11-26 11:57:32 +0530

    basic CRUD operation done

--- routes/post.js
+++ routes/post.js
@@ -7,42 +7,23 @@ const router = express.Router();
 const getCurrDate = () => moment().format("MM ddd, YYYY hh:mm:ss a");
 
 router.get("/", async (req, res) => {
-  //* test this
   const data = await Post.find()
     .exec()
     .catch(error => {
       console.log(error);
-      res.status(400).json({
-        status: "Bad request",
-      });
     });
 
-  if (data)
+  if (data) {
     res.json({
       status: "OK",
       data: data,
     });
+  } else {
+    res.status(400).json({
+      status: "Bad request",
+    });
+  }
 });
-
-router.get("/:id", (req, res) => {
-  //! recreate this
-  // Post.findById(req.params.id)
-  //   .exec()
-  //   .then(data => {
-  //     res.json({
-  //       status: "OK",
-  //       data: data,
-  //     });
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //     res.status(400).json({
-  //       status: "Bad request",
-  //     });
-  //   });
-});
-
-router.delete("/:id", async (req, res) => {});
 
 router.post("/", (req, res) => {
   const { title, content, tags } = req.body;
@@ -67,6 +48,65 @@ router.post("/", (req, res) => {
     .catch(error => {
       res.status(400).json({
         message: "Bad request",
+      });
+    });
+});
+
+router.get("/:id", async (req, res) => {
+  const data = await Post.findById(req.params.id)
+    .exec()
+    .catch(error => {
+      console.log(error);
+    });
+  if (data) {
+    res.json({
+      status: "OK",
+      data: data,
+    });
+  } else {
+    res.status(400).json({
+      status: "Bad request",
+    });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const data = req.body;
+  Post.findByIdAndUpdate(req.params.id, data)
+    .then(resData => {
+      if (resData) {
+        res.json({
+          status: "OK",
+          message: "Post updated",
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(400).json({
+        status: "Bad request",
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Post.findByIdAndRemove(req.params.id)
+    .then(data => {
+      if (data) {
+        res.json({
+          status: "OK",
+          message: "Post deleted",
+          data: data,
+        });
+      } else {
+        res.status(502).json({
+          status: "got an invalid response",
+        });
+      }
+    })
+    .catch(error => {
+      res.status(400).json({
+        status: "Bad request",
       });
     });
 });
